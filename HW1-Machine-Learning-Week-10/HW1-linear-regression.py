aaa3072f4e3d479b7cd8c5b453306ab4ae046005
Nguyễn Văn Đức <vanduc051103@gmail.com>
2023-11-29 23:48:56 +0700

    Create HW1-linear-regression.py

---
+++ HW1-Machine-Learning-Week-10/HW1-linear-regression.py
@@ -0,0 +1,73 @@
+import numpy as np
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
+
+
+class LinearRegression:
+
+    def __init__(self, init_theta=None, alpha=0.01, n_iter=100):
+        '''
+        Constructor
+        '''
+        self.alpha = alpha
+        self.n_iter = n_iter
+        self.theta = init_theta
+        self.JHist = None
+
+    def gradientDescent(self, X, y, theta):
+        '''
+        Fits the model via gradient descent
+        Arguments:
+            X is a n-by-d numpy matrix
+            y is an n-dimensional numpy vector
+            theta is a d-dimensional numpy vector
+        Returns:
+            the final theta found by gradient descent
+        '''
+        n, d = X.shape
+        self.JHist = []
+        for i in range(self.n_iter):
+            self.JHist.append((self.computeCost(X, y, theta), theta))
+            print("Iteration: ", i + 1, " Cost: ", self.JHist[i][0], " Theta: ", theta)
+            # Update theta using gradient descent
+            theta = theta - (self.alpha / n) * X.T @ (X @ theta - y)
+
+        return theta
+
+    def computeCost(self, X, y, theta):
+        '''
+        Computes the objective function
+        Arguments:
+          X is a n-by-d numpy matrix
+          y is an n-dimensional numpy vector
+          theta is a d-dimensional numpy vector
+        Returns:
+          a scalar value of the cost  
+              ** make certain you don't return a matrix with just one value! **
+        '''
+        n = len(y)
+        cost = (1 / (2 * n)) * np.sum((X @ theta - y) ** 2)
+        return cost
+
+    def fit(self, X, y):
+        '''
+        Trains the model
+        Arguments:
+            X is a n-by-d numpy matrix
+            y is an n-dimensional numpy vector
+        '''
+        n, d = X.shape
+        if self.theta is None:
+            self.theta = np.matrix(np.zeros((d, 1)))
+        self.theta = self.gradientDescent(X, y, self.theta)
+
+    def predict(self, X):
+        '''
+        Used the model to predict values for each instance in X
+        Arguments:
+            X is a n-by-d numpy matrix
+        Returns:
+            an n-dimensional numpy vector of the predictions
+        '''
+        predictions = X @ self.theta
+        return predictions
